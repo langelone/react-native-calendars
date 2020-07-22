73499464053193b8c7c867a74c26c6546c78269a
Lorenzo Angelone <l.angelone@kapusons.it>
2020-07-22 04:03:06 +0200

    removed onDayChange due current project

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -302,17 +302,18 @@ export default class AgendaView extends Component {
   }
 
   onDayChange(day) {
-    const newDate = parseDate(day);
-    const withAnimation = dateutils.sameMonth(newDate, this.state.selectedDay);
+    // rimosso per lo scopo del progetto
+    // const newDate = parseDate(day);
+    // const withAnimation = dateutils.sameMonth(newDate, this.state.selectedDay);
     
-    this.calendar.scrollToDay(day, this.calendarOffset(), withAnimation);
-    this.setState({
-      selectedDay: parseDate(day)
-    });
+    // this.calendar.scrollToDay(day, this.calendarOffset(), withAnimation);
+    // this.setState({
+    //  selectedDay: parseDate(day)
+    // });
 
-    if (this.props.onDayChange) {
-      this.props.onDayChange(xdateToData(newDate));
-    }
+    // if (this.props.onDayChange) {
+    //  this.props.onDayChange(xdateToData(newDate));
+    //}
   }
 
   generateMarkings() {
